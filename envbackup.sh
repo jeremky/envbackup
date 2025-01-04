7871b55bc226d2e7f29b16c5bf6cbd129d153837
JeremKy <jeremky@me.com>
2025-01-04 13:31:39 +0100

    Mise à jour

--- envbackup.sh
+++ envbackup.sh
@@ -2,6 +2,7 @@
 set -e
 
 dir=$(dirname "$0")
+list="$dir/envbackup.lst"
 
 ## Verification du user
 if [ "$USER" = "root" ] ; then
@@ -12,13 +13,13 @@ fi
 ## Copie des configurations OS
 if [ ! -f $HOME/.*_aliases ] || [ "$1" = "r" ] ; then
   if [ -d $dir/dotfiles ] ; then
-    cd $dir/dotfiles
-    cp -Rp $dir/dotfiles $HOME
+    for file in $(cat $list | grep -v '#') ; do
+      cp -Rp $dir/dotfiles/$file $HOME
+    done
     echo "Restauration effectuée"
   fi
 else
   cd $HOME
-  list="$dir/$(basename -s .sh $0).lst"
   for file in $(cat $list | grep -v '#') ; do
     if [ -f $HOME/$file ] || [ -d $HOME/$file ] ; then
       cp -Rp $file $dir/dotfiles
