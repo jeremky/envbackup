40009532f87bfd961921166d292da3c775f99396
JeremKy <jeremky@me.com>
2025-03-10 21:27:10 +0100

    Mise à jour

--- envbackup.sh
+++ envbackup.sh
@@ -5,14 +5,14 @@ dir=$(dirname "$0")
 list="$dir/envbackup.lst"
 
 ## Verification du user
-if [ "$USER" = "root" ] ; then
+if [[ "$USER" = "root" ]]; then
   echo "Ne pas lancer en tant que root !"
   exit 0
 fi
 
 ## Copie des configurations OS
-if [ ! -f $HOME/.*_aliases ] || [ "$1" = "r" ] ; then
-  if [ -d $dir/dotfiles ] ; then
+if [[ ! -f $HOME/.*_aliases ]] || [[ "$1" = "r" ]]; then
+  if [[ -d $dir/dotfiles ]]; then
     for file in $(cat $list | grep -v '#') ; do
       cp -Rp $dir/dotfiles/$file $HOME
     done
@@ -21,7 +21,7 @@ if [ ! -f $HOME/.*_aliases ] || [ "$1" = "r" ] ; then
 else
   cd $HOME
   for file in $(cat $list | grep -v '#') ; do
-    if [ -f $HOME/$file ] || [ -d $HOME/$file ] ; then
+    if [[ -f $HOME/$file ]] || [[ -d $HOME/$file ]]; then
       cp -Rp $file $dir/dotfiles
     fi
   done
