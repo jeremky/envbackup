d7c4a1948b70c056a55b6ac69bc13a6e173854dc
JeremKy <jeremky@me.com>
2025-06-14 15:27:47 +0200

    Personnalisation des fichiers selon la distribution

--- envbackup.sh
+++ envbackup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
 dir=$(dirname "$0")
-list="$dir/envbackup.lst"
+dist=$(cat /etc/os-release | grep "^ID" | cut -d= -f2,2)
+list="$dir/config/$dist.lst"
 
 # Couleurs
 GREEN='\033[0;32m'
@@ -16,17 +17,17 @@ fi
 
 # Copie des configurations OS
 if [[ ! -f $HOME/.bash_aliases || "$1" = "r" ]]; then
-  if [[ -d $dir/dotfiles ]]; then
-    cp -Rp $dir/dotfiles/.* $HOME
+  if [[ -d $dir/dotfiles/$dist ]]; then
+    cp -Rp $dir/dotfiles/$dist/.* $HOME
     sed -i "s,^scripts=.*,scripts=$(realpath "$0" | rev | cut -d/ -f3- | rev)," ~/.bash_aliases
     echo -e "${GREEN}Restauration effectuée${RESET}"
   fi
 else
   for file in $(cat $list | grep -v '#'); do
     if [[ -f $HOME/$file || -d $HOME/$file ]]; then
-      rm -fr $dir/dotfiles/$file
-      mkdir -p $(dirname $dir/dotfiles/$file)
-      cp -Rp $HOME/$file $dir/dotfiles/$file
+      rm -fr $dir/dotfiles/$dist/$file
+      mkdir -p $(dirname $dir/dotfiles/$dist/$file)
+      cp -Rp $HOME/$file $dir/dotfiles/$dist/$file
     fi
   done
   echo -e "${GREEN}Sauvegarde effectuée${RESET}"
