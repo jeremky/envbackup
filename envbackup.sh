ba423a76b1a517371d16cf71c569d9d321cbdd70
JeremKy <jeremky@me.com>
2025-07-21 11:11:40 +0200

    Mise à jour

--- envbackup.sh
+++ envbackup.sh
@@ -4,7 +4,7 @@ dir=$(dirname "$(realpath "$0")")
 dist=$(grep "^ID=" /etc/os-release | cut -d= -f2,2 | tr -d '"')
 list="$dir/config/$dist.lst"
 
-# Messages colorisés
+# Messages en couleur
 error()    { echo -e "\033[0;31m====> $*\033[0m" ;}
 message()  { echo -e "\033[0;32m====> $*\033[0m" ;}
 warning()  { echo -e "\033[0;33m====> $*\033[0m" ;}
