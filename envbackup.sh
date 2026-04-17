874dcabe6373b9ebd9255b0f394b1552bb0be0d5
JeremKy <jeremky@me.com>
2026-04-17 11:47:24 +0200

    Update

--- envbackup.sh
+++ envbackup.sh
@@ -10,7 +10,7 @@ message() { echo -e "\033[0;32m====> $*\033[0m"; }
 warning() { echo -e "\033[0;33m====> $*\033[0m"; }
 
 # Vérification du user
-if [[ "$USER" = "root" ]]; then
+if [[ "$EUID" -eq 0 ]]; then
   error "Ne pas lancer en tant que root !"
   exit 1
 fi
@@ -23,12 +23,12 @@ fi
 
 # Copie des configurations OS
 if [[ "$1" = "r" ]]; then
-  if [[ -d $dir/dotfiles/$dist ]]; then
+  if [[ -d "$dir/dotfiles/$dist" ]]; then
     cp -Rp $dir/dotfiles/$dist/.* $HOME
     warning "Restauration effectuée"
   fi
 else
-  grep -v '^ *#' $list | while read -r line; do
+  grep -v '^ *#' "$list" | while read -r line; do
     if [[ -e "$HOME/$line" ]]; then
       rm -fr "$dir/dotfiles/$dist/$line"
       mkdir -p "$(dirname $dir/dotfiles/$dist/$line)"
