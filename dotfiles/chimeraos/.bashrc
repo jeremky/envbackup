c504bbd59a75dfcbaed54baf56ea8e0734c7c0b8
JeremKy <jeremky@me.com>
2026-05-25 14:01:07 +0200

    Ajout de la version initiale de envbackup

---
+++ dotfiles/chimeraos/.bashrc
@@ -0,0 +1,33 @@
+# ~/.bashrc
+
+# If not running interactively, don't do anything
+case $- in
+  *i*) ;;
+  *) return ;;
+esac
+
+# don't put duplicate lines
+HISTCONTROL=ignoreboth
+
+# append to the history file
+shopt -s histappend
+
+# for setting history length
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+# check the window size after each command
+shopt -s checkwinsize
+
+# Alias definitions.
+if [ -f ~/.bash_aliases ]; then
+  . ~/.bash_aliases
+fi
+
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+  xterm* | rxvt*)
+    PS1="\[\e]0;\u@\h: \w\a\]$PS1"
+    ;;
+  *) ;;
+esac
---
+++ envbackup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -e
+
+dir=$(dirname "$(realpath "$0")")
+dist=$(grep "^ID=" /etc/os-release | cut -d= -f2 | tr -d '"')
+list="$dir/config/$dist.cfg"
+
+# Messages en couleur
+error() { echo -e "\033[0;31m====> $*\033[0m"; }
+message() { echo -e "\033[0;32m====> $*\033[0m"; }
+warning() { echo -e "\033[0;33m====> $*\033[0m"; }
+
+# Vérification du user
+if [[ "$EUID" -eq 0 ]]; then
+  error "Ne pas lancer en tant que root !"
+  exit 1
+fi
+
+# Vérification du fichier de list
+if [[ ! -f "$list" ]]; then
+  error "Fichier $list absent !"
+  exit 1
+fi
+
+# Copie des configurations OS
+if [[ "$1" = "r" ]]; then
+  if [[ -d "$dir/dotfiles/$dist" ]]; then
+    cp -Rp $dir/dotfiles/$dist/.* $HOME
+    warning "Restauration effectuée"
+  fi
+else
+  grep -v '^ *#' "$list" | while read -r line; do
+    if [[ -e "$HOME/$line" ]]; then
+      rm -fr "$dir/dotfiles/$dist/$line"
+      mkdir -p "$(dirname $dir/dotfiles/$dist/$line)"
+      cp -Rp "$HOME/$line" "$dir/dotfiles/$dist/$line"
+    fi
+  done
+  message "Sauvegarde effectuée"
+fi
